0fb899129f8339bb06bf466f150db7f33756a962
Eike Wulff <e.wulff@aleron.eu>
2016-01-30 17:46:19 +0100

    tilt und movement gebaut

---
+++ Again/Assets/MoveWithInertia.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Tilt))]
+public class MoveWithInertia : MonoBehaviour
+{
+    private Transform target;
+    private Tilt tilt;
+    private float speed = 0;
+    // Use this for initialization
+    void Start()
+    {
+        tilt = GetComponent<Tilt>();
+        target = GetComponent<Transform>();   
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.acceleration.x);
+        speed = speed + Input.acceleration.x;
+        target.Translate(speed * Time.deltaTime, 0, 0);
+        tilt.angle = speed * 10;
+    }
+}
