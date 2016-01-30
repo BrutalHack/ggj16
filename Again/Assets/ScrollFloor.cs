db019f64ffaaee329030ca732f39567670f26a25
Eike Wulff <e.wulff@aleron.eu>
2016-01-30 15:49:03 +0100

    hingejoint stuff

---
+++ Again/Assets/ScrollFloor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollFloor : MonoBehaviour
+{
+    public float speed = 1;
+    public Material floorMaterial;
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {   
+        Vector2 oldOffset = floorMaterial.GetTextureOffset("_MainTex");
+
+        floorMaterial.SetTextureOffset("_MainTex", new Vector2(0, oldOffset.y - Time.deltaTime * speed));
+    }
+}
